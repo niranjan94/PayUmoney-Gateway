b9ef4a6361fb180c010529f53ccf34cf0aee3b49
Niranjan Rajendran <niranjan94@yahoo.com>
2015-01-28 20:28:02 +0530

    Added Readme, LICENSE and set up constructor, getters and setters.

---
+++ README.md
@@ -0,0 +1,20 @@
+## PayUMoney Payment Gateway
+
+An easy to use library for the PayUMoney Payment Gateway
+
+
+## Copyright
+
+    Copyright 2015 Niranjan Rajendran. All rights reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- src/Payment.php
+++ src/Payment.php
@@ -2,8 +2,48 @@
 namespace Payment;
 class Payment {
 
-    function __construct()
-    {
+    private $productionServer = "https://secure.payu.in/_payment";
+    private $testingServer = "https://test.payu.in/_payment";
+    private $isProduction = false;
 
+    private $testData = array(
+        "key" => "JBZaLc",
+        "salt" => "GQs7yium"
+    );
+
+    private $key;
+    private $salt;
+
+    function __construct($key = "", $salt = "")
+    {
+        if($key==null||$salt==null||trim($key)==""||trim($salt) ==""){
+            $this->key = $this->testData['key'];
+            $this->salt = $this->testData['salt'];
+            $this->isProduction = false;
+        } else {
+            $this->isProduction = true;
+        }
     }
+
+    public function getKey()
+    {
+        return $this->key;
+    }
+
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    }
+
+
 }
